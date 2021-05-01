d701b7c676ad52b778429a340ffc0621b1e2762e
rata854 <nitiham2006@gmail.com>
2021-05-01 06:14:19 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0a7c2e5e98678fca61a3b395b4aaa5cd2da89d8ff51f13f882aa5eda35bb3223a14f3371859b6bd8d46c9583948011e8d9e2d13d2112eec1dc133a4871feefb5'
