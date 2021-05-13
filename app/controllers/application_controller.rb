b343ae3e1debfe67558e6f00b07ef4b9414450dd
rata854 <nitiham2006@gmail.com>
2021-05-13 05:30:03 +0000

    [Add]細部調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!,except: [:top]
-  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     post_images_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
 
   protected
@@ -12,5 +15,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  
   devise_for :users
-  root 'post_images#index'
+  root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
